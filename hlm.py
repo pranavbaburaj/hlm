7bfcf8cf1c31cfac8069c372ea5020ca0479c732
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-02-05 14:12:07 +0530

    uninstall

--- hlm.py
+++ hlm.py
@@ -21,7 +21,10 @@ class Installer():
 		if self.data not in os.listdir(self.mod):
 			try:
 				req = requests.get(url)
-				self.write_file(req.content)
+				if req.content == b'404: Not Found':
+					print(color.red(f"Cannot find {self.data}"))
+				else:
+					self.write_file(req.content)
 			except:
 				print(color.red("An error occured"))
 		else:
@@ -31,12 +34,29 @@ class Installer():
 		with open(os.path.join(self.mod, self.data), "wb") as file:
 			file.write(data)
 
+class Uninstall():
+	def __init__(self, name):
+		self.name = f"{name}.has"
+		self.mod = os.path.join(os.getcwd(), "hlib_modules")
+
+		self.final_path = os.path.join(self.mod, self.name)
+
+		self.delete()
+
+	def delete(self):
+		if os.path.exists(self.final_path):
+			os.remove(self.final_path)
+		else:
+			print(color.red(f"Cannot find {self.final_path}"))
+
 @click.command()
 @click.argument('command', type=str)
 @click.argument('name', type=str)
 def index(command, name):
     if command == "install":
     	i = Installer(name)
+    elif command == "uninstall" or command == "remove":
+    	u = Uninstall(name)
 
 
 if __name__ == "__main__":
